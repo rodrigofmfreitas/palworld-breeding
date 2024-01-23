84a10dfa63b0a9e8b103433beafd014141a041e6
Rodrigo Melo <digofmf@gmail.com>
2024-01-23 19:35:05 -0300

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ df = pd.read_csv("dataframe.csv", index_col=1).drop(columns=["Unnamed: 0"])
 
 pals = list(df.keys())
 pal = st.selectbox("Check Pal options", pals)
-pal_df = df[df[df.keys()] == pal].dropna(how="all").dropna(how="all", axis=1)
+pal_df = df[df[df.keys()] == pal]
+pal_df = pal_df.dropna(how="all").dropna(how="all", axis=1)
 
 pal_df
