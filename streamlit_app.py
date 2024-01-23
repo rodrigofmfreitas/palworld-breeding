9a9d0ba556665e0659fe07fea948a03a6f5f8880
Rodrigo Melo <digofmf@gmail.com>
2024-01-23 19:33:52 -0300

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from IPython.display import display
 df = pd.read_csv("dataframe.csv", index_col=1).drop(columns=["Unnamed: 0"])
 
 pals = list(df.keys())
-pal = st.multiselect("Check Pal options", pals)
+pal = st.selectbox("Check Pal options", pals)
 pal_df = df[df[df.keys()] == pal].dropna(how="all").dropna(how="all", axis=1)
 
 pal_df
