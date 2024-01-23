e6c2ad3596c34913f82c1cbfabd6bda4eb1c10ac
Rodrigo Melo <digofmf@gmail.com>
2024-01-23 19:25:56 -0300

    s

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+from IPython.display import display
+
+df = pd.read_csv("dataframe.csv", index_col=1).drop(columns=["Unnamed: 0"])
+
+pals = list(df.keys())
+pal = st.multiselect("Check Pal options", pals)
+pal_df = df[df[df.keys()] == pal].dropna(how="all").dropna(how="all", axis=1)
+
+display(pal_df)
