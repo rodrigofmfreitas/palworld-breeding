67a266b659eeef6b2ee9df2c7ecebed719c374fb
Rodrigo Melo <digofmf@gmail.com>
2024-01-24 02:27:02 -0300

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,15 @@ df = pd.read_csv("dataframe.csv", index_col=1).drop(columns=["Unnamed: 0"])
 pals = list(df.keys())
 pal = st.selectbox("Check Pal options", pals)
 
-pal_df = create_dataframe(pal, df)
+df1 = df[df[df.keys()] == pal].dropna(how="all").dropna(how="all", axis=1)
+pal_dict = df1.to_dict()
 
-pal_df
+pal_tuples = []
+for i in pal_dict:
+    for j in pal_dict[i]:
+        if pal_dict[i][j] == "Suzaku":
+            pal_tuples.append((i, j))
+res = list({*map(tuple, map(sorted, pal_tuples))})
+df = pd.DataFrame(res, columns=["Parent 1", "Parent 2"])
+
+df
