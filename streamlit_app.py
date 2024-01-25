8ae667816e4804567fb93fca7b16c62125e94c31
Rodrigo Melo <digofmf@gmail.com>
2024-01-25 09:54:33 -0300

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-from create_dataframe import create_dataframe
-
 df = pd.read_csv("dataframe.csv", index_col=1).drop(columns=["Unnamed: 0"])
 
 pals = list(df.keys())
